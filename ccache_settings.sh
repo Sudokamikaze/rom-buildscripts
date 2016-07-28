408f53a07a4b143c4fb9129885cb48448b81c389
sudokamikaze <pulshencode@outlook.com>
2016-07-28 05:07:48 +0300

    Created spaces before ccache commands

--- ccache_settings.sh
+++ ccache_settings.sh
@@ -11,10 +11,12 @@ echo ================================
 echo -n "Choose an action: "
 read menu
 case "$menu" in
-  1) prebuilts/misc/linux-x86/ccache/ccache -s
+  1) echo " " 
+  prebuilts/misc/linux-x86/ccache/ccache -s
   ;;
   2) echo -n "Enter size in GB: "
   read size
+  echo " "
   prebuilts/misc/linux-x86/ccache/ccache -M "$size"G
   ;;
   *) echo Error
