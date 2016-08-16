de4c2f710e3e0c1cddb3b95a67c81b6ba72e388f
sudokamikaze <pulshencode@outlook.com>
2016-08-16 05:47:28 +0300

    Added option cleanup ccache

--- ccache_settings.sh
+++ ccache_settings.sh
@@ -12,7 +12,8 @@ export CCACHE_DIR="$CACHEDIRPATH".ccache
 
 echo ================================
 echo "1. Check CCACHE size"
-echo "2. Define new CCACHE max size"
+echo "2. Cleanup CCACHE"
+echo "3. Define new CCACHE max size"
 echo ================================
 echo -n "Choose an action: "
 read menu
@@ -20,7 +21,10 @@ case "$menu" in
   1) echo " "
   prebuilts/misc/linux-x86/ccache/ccache -s
   ;;
-  2) echo -n "Enter size in GB: "
+  2) echo "Cleaning ccache"
+  prebuilts/misc/linux-x86/ccache/ccache -c
+  ;;
+  3) echo -n "Enter size in GB: "
   read size
   echo " "
   prebuilts/misc/linux-x86/ccache/ccache -M "$size"G
