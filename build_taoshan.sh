c72184bfca94c27d6f924a16a78661537c11cb42
Sudokamikaze <pulshencode@outlook.com>
2016-08-31 17:13:08 +0300

    Moved vars to if archlinux checker

--- build_taoshan.sh
+++ build_taoshan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# There vars for ARCHLINUX python2 setup
-PWD=$(pwd)
-BASETOPDIR=$PWD
+
 
 # ==================TUNABLE==================
 export BLOCK_BASED_OTA=false # Remove this string if you don't want to disable BLOCK Baseds SHIT!
@@ -15,6 +13,8 @@ CCACHEENABLE=true # Define true if u want to use ccache / Define false if u don'
 . build/envsetup.sh
 
 if [ $IFARCHLINUX == true ]; then
+PWD=$(pwd)
+BASETOPDIR=$PWD
 virtualenv2 venv
 ln -s /usr/lib/python2.7/* "$BASETOPDIR"/venv
 source venv/bin/activate
