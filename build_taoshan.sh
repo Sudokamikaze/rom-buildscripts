e4d3e9f65d80f06f6cb97c681893cad3f4bfacee
Sudokamikaze <pulshencode@outlook.com>
2016-08-31 17:11:11 +0300

    Created var for checking dir

--- build_taoshan.sh
+++ build_taoshan.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# There vars for ARCHLINUX python2 setup
+PWD=$(pwd)
+BASETOPDIR=$PWD
+
 # ==================TUNABLE==================
 export BLOCK_BASED_OTA=false # Remove this string if you don't want to disable BLOCK Baseds SHIT!
 CACHEDIRPATH=/ccache/RR/ # Define your dir for ccache here
@@ -12,7 +16,7 @@ CCACHEENABLE=true # Define true if u want to use ccache / Define false if u don'
 
 if [ $IFARCHLINUX == true ]; then
 virtualenv2 venv
-ln -s /usr/lib/python2.7/* /home/$USER/Git/tmp/RR/venv/lib/python2.7/
+ln -s /usr/lib/python2.7/* "$BASETOPDIR"/venv
 source venv/bin/activate
 fi
 
