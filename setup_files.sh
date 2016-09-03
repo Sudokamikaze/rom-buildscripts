5b7aa7aa6a3a173eebaec34aa0465711bd7e5450
Sudokamikaze <pulshencode@outlook.com>
2016-09-03 22:28:06 +0300

    Initial support grouper in manifest

--- setup_files.sh
+++ setup_files.sh
@@ -6,7 +6,7 @@ echo -n "Setup vendor and device specific files? [Y/N] "
 read menu
 case "$menu" in
   y|Y) echo "Creating manifest..."
-  cp local_manifests/taoshan.xml .repo/local_manifests/roomservice.xml
+  cp local_manifests/roomservice.xml .repo/local_manifests/roomservice.xml
   echo "Setuping vendor files..."
   cd vendor
   git clone https://github.com/TheMuppets/proprietary_vendor_sony.git sony -b cm-13.0
