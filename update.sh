fe887bb1a4b1179d7497efc00d99fd8b150245eb
Sudokamikaze <pulshencode@outlook.com>
2016-09-03 15:56:44 +0300

    Bugfix

--- update.sh
+++ update.sh
@@ -2,18 +2,12 @@
 
 function upsources {
   make clean
-  repo sync --force-sync
-}
-
-function updevice {
-  cd device/sony && rm -rf taoshan
-  git clone https://github.com/Sudokamikaze/android_device_sony_taoshan.git taoshan
-  cd ../../
+  repo sync -f --force-sync
 }
 
 function upvendor {
-  cd vendor/sony && rm -rf taoshan
-  git clone https://github.com/Sudokamikaze/proprietary_vendor_sony.git sony -b taoshan
+  cd vendor&& rm -rf sony
+  git clone https://github.com/TheMuppets/proprietary_vendor_sony.git sony
   cd ../
 }
 
@@ -21,10 +15,9 @@ function upvendor {
 echo " "
 echo " "
 echo ========================================
-echo "1. Update sources"
-echo "2. Update device files"
-echo "3. Update vendor files"
-echo "4. Update all(sources,device,vendor)"
+echo "1. Update sources(including device files)"
+echo "2. Update vendor files"
+echo "3. Update all(sources,vendor)"
 echo ========================================
 echo -n "Choose an option: "
 read menu
@@ -32,21 +25,15 @@ case "$menu" in
   1) echo "Updating sources"
   upsources
   ;;
-  2) echo "Updating device files..."
-  updevice
-  breakfast cm_taoshan-userdebug
-  echo "Done!"
-  ;;
-  3) echo "Updating vendor files..."
+  2) echo "Updating vendor files..."
   upvendor
-  breakfast cm_taoshan-userdebug
+  breakfast taoshan
   echo "Done!"
   ;;
-  4) echo "Updating all"
+  3) echo "Updating all"
   upsources
-  updevice
   upvendor
-  breakfast cm_taoshan-userdebug
+  breakfast taoshan
   echo "Done!"
   ;;
   *) echo "Unknown symbol.."
