e8e6555eeee1898e2a3b737ed54986b7bf74b097
Sudokamikaze <pulshencode@outlook.com>
2016-09-05 01:47:22 +0300

    Bugfix

--- setup_files.sh
+++ setup_files.sh
@@ -6,6 +6,7 @@ echo "======================"
 echo "1. Nexus 7(grouper)"
 echo "2. Xperia L(taoshan)"
 echo "3. Both"
+echo "======================"
 echo -n "Select the device: "
 read choise
 case "$choise" in
@@ -26,18 +27,18 @@ echo -n "Setup vendor and device specific files? [Y/N] "
 read menu
 case "$menu" in
   y|Y) echo "Creating manifest..."
-  cp local_manifests/roomservice.xml .repo/local_manifests/roomservice.xml
+  mv local_manifests .repo/
   echo "Setuping vendor files..."
   cd vendor
-  if [ $device=grouper ]; then
+  if [ $device == grouper ]; then
   git clone https://github.com/TheMuppets/proprietary_vendor_asus.git asus -b cm-13.0
   cd ../
   breakfast grouper
-elif [ $device=taoshan ]; then
+elif [ $device == taoshan ]; then
   git clone https://github.com/TheMuppets/proprietary_vendor_sony.git sony -b cm-13.0
   cd ../
   breakfast taoshan
-elif [ $device=both ]; then
+elif [ $device == both ]; then
   git clone https://github.com/TheMuppets/proprietary_vendor_sony.git sony -b cm-13.0
   git clone https://github.com/TheMuppets/proprietary_vendor_asus.git asus -b cm-13.0
   cd ..
