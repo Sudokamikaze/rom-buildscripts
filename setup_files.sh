e3b60fb32a1be21430ad72fd38fee61e63880c08
Sudokamikaze <pulshencode@outlook.com>
2016-09-15 18:57:50 +0300

    Support for 5.1 setup

--- setup_files.sh
+++ setup_files.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 . build/envsetup.sh
 
-#  EDIT THERE !!
-romver=default # If u define there "lp" it will download patch to enable/disable block based from cm 13
-# ==============
-
-function blockpatch{
+function blockpatch {
 if [ $romver == lp ]; then
 curl -O https://github.com/CyanogenMod/android_build/commit/fffc2a16c61077abf583df87f94000356f172b77.patch
 cd build/core
@@ -13,15 +9,18 @@ patch < ../../fffc2a16c61077abf583df87f94000356f172b77.patch
 fi
 }
 
-function prepare{
-  mv local_manifests .repo/
+function prepare {
+  mkdir -p .repo/local_manifests
+  if [ $romver == lp ]; then
+  mv local_manifests/roomservice_lp.xml .repo/roomservice.xml
+elif [ $romver == mm ]; then
+  mv local_manifests/roomservice_mm.xml .repo/roomservice.xml
+fi
   repo sync -j 5 --force-sync
   case "$device" in
   grouper) breakfast grouper
   ;;
   taoshan) breakfast taoshan
-  ;;
-  both) breakfast taoshan && breakfast grouper
   ;;
 esac
 }
@@ -32,7 +31,6 @@ echo " "
 echo "======================"
 echo "1. Nexus 7(grouper)"
 echo "2. Xperia L(taoshan)"
-echo "3. Both"
 echo "======================"
 echo -n "Select the device: "
 read choise
@@ -43,8 +41,28 @@ case "$choise" in
   2) echo "Selected taoshan"
   device=taoshan
   ;;
-  3) echo "Selected both"
-  device=both
+  *) echo Error
+  ;;
+esac
+
+echo "Select manifest version"
+echo "======================"
+if [ $device == grouper ]; then
+echo "1. MM(6.0.1)"
+echo "Grouper have very poor performance"
+echo "on LP(5.1)"
+fi
+echo "1. MM(6.0.1)"
+echo "2. LP(5.1)"
+echo "======================"
+echo -n "Select the version: "
+read choise
+case "$choise" in
+  1) echo "Selected MM"
+  romver=mm
+  ;;
+  2) echo "Selected LP"
+  romver=lp
   ;;
   *) echo Error
   ;;
