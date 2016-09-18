573a33f8174045de74e97778a3c126fb3fc12456
Sudokamikaze <pulshencode@outlook.com>
2016-09-18 12:47:31 +0300

    Removed buggy sudocore and other functional

--- build_rom.sh
+++ build_rom.sh
@@ -6,19 +6,12 @@ CACHEDIRPATH=/ccache/android/ # Define your dir for ccache here
 CCACHESIZE=10 # Define size of cache in GB, e.x CCACHESIZE=15 or CCACHESIZE=20 without "G" letter
 IFARCHLINUX=true # Define true if your distro IS ArchLinux/ Define false if your distro NOT ArchLinux
 CCACHEENABLE=true # Define true if u want to use ccache / Define false if u don't wand ccache
-INTELCORECPU=false # Define here if your CPU are Intel Core i3/i5/i7 sandy-bridge or newer
 export OUT_DIR_COMMON_BASE=/mnt/hdd/out # Define there or comment this var
 # ===========================================
 
 # ==================DEVICE===================
 CURRENTDEVICE=taoshan # Define here build device. E.x CURRENTDEVICE=grouper or taoshan
 # ===========================================
-
-rm /tmp/building_rom.pid
-touch /tmp/building_rom.pid
-echo "Starting sudocore..."
-sudo ./sudocore.sh &
-sleep 10s
 
 # PREPARE STAGE
 . build/envsetup.sh
@@ -46,5 +39,3 @@ case "$CURRENTDEVICE" in
   ;;
   *) echo "Error, corrent typo"
 esac
-
-rm /tmp/building_rom.pid
