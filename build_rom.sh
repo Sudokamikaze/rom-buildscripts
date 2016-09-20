ef2aa2afaa62b0a463db1cb1e0a4bd499dc74e74
Sudokamikaze <pulshencode@outlook.com>
2016-09-20 21:35:47 +0300

    Correct build section and commend var for out/ at extrenal hdd

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ CACHEDIRPATH=/ccache/android/ # Define your dir for ccache here
 CCACHESIZE=10 # Define size of cache in GB, e.x CCACHESIZE=15 or CCACHESIZE=20 without "G" letter
 IFARCHLINUX=true # Define true if your distro IS ArchLinux/ Define false if your distro NOT ArchLinux
 CCACHEENABLE=true # Define true if u want to use ccache / Define false if u don't wand ccache
-export OUT_DIR_COMMON_BASE=/mnt/hdd/out # Define there or comment this var
+# export OUT_DIR_COMMON_BASE=/mnt/hdd/out # Define there or comment this var
 # ===========================================
 
 # ==================DEVICE===================
@@ -33,9 +33,9 @@ fi
 # BUILD STAGE
 croot
 case "$CURRENTDEVICE" in
-  taoshan) breakfast taoshan && CFLAGS='-O2 -fomit-frame-pointer' brunch taoshan
+  taoshan) breakfast taoshan && CFLAGS='-O2 -fomit-frame-pointer' mka bacon
   ;;
-  grouper) breakfast grouper && CFLAGS='-O2 -fomit-frame-pointer' brunch grouper
+  grouper) breakfast grouper && CFLAGS='-O2 -fomit-frame-pointer' mka bacon
   ;;
   *) echo "Error, corrent typo"
 esac
