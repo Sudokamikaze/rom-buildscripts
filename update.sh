a42e6f03cab265d219b965acb76891d3c4cf8b81
Sudokamikaze <pulshencode@outlook.com>
2016-09-27 16:34:18 +0300

    Added feature to update local_manifests

--- update.sh
+++ update.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+eval $(grep CURRENTDEVICE= ./build_rom.sh)
+eval $(grep CURRENTVERSION= ./build_rom.sh)
+
 function upsources {
   make clean && make clobber
   repo sync -j 5 -f --force-sync
 }
 
-echo "Do you want to update?: "
-read item
-if [ $item == y ]; then
-upsources
-elif [ $item == Y ]; then
-upsources
-else
-exit
+function upmanifests {
+if [ $CURRENTDEVICE == grouper ]; then
+  case "$CURRENTVERSION" in
+    lp) echo "Error, grouper LP manifest is unsuported"
+    echo "Please edit your build_rom.sh"
+    exit
+    ;;
+  esac
 fi
+cd .repo/local_manifests
+rm roomservice.xml
+curl -O https://raw.githubusercontent.com/Zeroskies/local_manifests/master/roomservice_"$CURRENTVERSION"_"$CURRENTDEVICE".xml
+}
+
+echo "============================"
+echo "1. Update sources"
+echo "2. Update local_manifests"
+echo "============================"
+echo -n "Choose an action: "
+read choise
+case "$choise" in
+  1) upsources
+  ;;
+  2) upmanifests
+  ;;
+  *) echo "Unknown symbol."
+  exit
+  ;;
+esac
