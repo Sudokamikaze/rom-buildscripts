b65aaa8b61174124d0f169f278b8ef68c1ed5aad
Sudokamikaze <pulshencode@outlook.com>
2016-12-11 13:36:10 +0200

    Create check for building

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ CACHEDIRPATH=/ccache/android/ # Define your dir for ccache here
 CCACHESIZE=10 # Define size of cache in GB, e.x CCACHESIZE=15 or CCACHESIZE=20 without "G" letter
 IFARCHLINUX=true # Define true if your distro IS ArchLinux/ Define false if your distro NOT ArchLinux
 CCACHEENABLE=true # Define true if u want to use ccache / Define false if u don't wand ccache
-BUILDKITKAT=false # Define true here if you building 4.4.4 and you are on archlinux(install make-3.81 and jre6 from aur)
+BUILDKITKAT=auto # Define true here if you building 4.4.4 and you are on archlinux(install make-3.81 and jre6 from aur)
 # export OUT_DIR_COMMON_BASE=/mnt/hdd/out # Define there or comment this var
 # ===========================================
 
@@ -42,11 +42,19 @@ export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_
 prebuilts/misc/linux-x86/ccache/ccache -M "$CCACHESIZE"G
 fi
 
-if [ $BUILDKITKAT == true ]; then
+if [ "$BUILDKITKAT" == "true" ]; then
 pwdvar=$(pwd)
 export PATH="/usr/lib/jvm/java-6-jre/jre/bin:$PATH"
 export JAVA_HOME=/usr/lib/jvm/java-6-jre/jre
 export PATH="$pwdvar/makedir:$PATH"
+elif [ "$BUILDKITKAT" == "auto" ]; then
+checkdir=$(ls | grep makedir)
+if [ "$checkdir" == "makedir" ]; then
+pwdvar=$(pwd)
+export PATH="/usr/lib/jvm/java-6-jre/jre/bin:$PATH"
+export JAVA_HOME=/usr/lib/jvm/java-6-jre/jre
+export PATH="$pwdvar/makedir:$PATH"
+fi
 fi
 
 # BUILD STAGE
