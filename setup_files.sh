79255e97192165f87739de27ffa87827cd733a73
Sudokamikaze <pulshencode@outlook.com>
2016-12-15 23:26:27 +0200

    Some rewrites

--- setup_files.sh
+++ setup_files.sh
@@ -10,7 +10,7 @@ function pythonvenv {
   ln -s /usr/lib/python2.7/* "$BASETOPDIR"/venv/lib/python2.7/
 }
 
-function prepare {
+function manifestdownload {
   mkdir -p .repo/local_manifests
   cd .repo/local_manifests
   curl -O https://raw.githubusercontent.com/Zeroskies/local_manifests/master/"$gitmanifests".xml
@@ -25,55 +25,8 @@ esac
 cd ../../
 }
 
-function displaymenu {
-  echo "Select manifest version"
-  echo "======================"
-  if [ $device == grouper ]; then
-  echo "1. MM(6.0.1)"
-  echo "2. KK(4.4.4)"
-elif [ $device == both ]; then
-  echo "1. MM(6.0.1)"
-else
-  echo "1. MM(6.0.1)"
-  echo "2. LP(5.1)"
-fi
-  echo "======================"
-  echo -n "Select the version: "
-  read choise
-  if [ $device == grouper ]; then
-    case "$choise" in
-      1) echo "Selected MM"
-      romver=mm
-      ;;
-      2) echo "Selected KK"
-      romver=kk
-      ;;
-      *) echo Error
-      ;;
-    esac
-elif [ $device == both ]; then
-    case "$choise" in
-      1) echo "Selected MM"
-      romver=mm
-      ;;
-      *) echo Error
-      ;;
-    esac
-else
-  case "$choise" in
-    1) echo "Selected MM"
-    romver=mm
-    ;;
-    2) echo "Selected LP"
-    romver=lp
-    ;;
-    *) echo Error
-    ;;
-  esac
-fi
-}
 
-function prepareman {
+function manifestprepare {
 if [ $device == grouper ]; then
 case "$romver" in
   mm) gitmanifests=roomservice_mm_grouper
@@ -90,18 +43,14 @@ case "$romver" in
   lp) gitmanifests=roomservice_lp_taoshan
   ;;
 esac
-elif [ $device == both ]; then
- gitmanifests=roomservice_mm_both
 fi
 }
-
 
 echo " "
 echo " "
 echo "================================"
 echo "1. Nexus 7(grouper)"
 echo "2. Xperia L(taoshan)"
-echo "3. Both(Xperia/Nexus)"
 echo "================================"
 echo "fixpython. Recreate python venv"
 echo "================================"
@@ -113,9 +62,6 @@ case "$choise" in
   ;;
   2) echo "Selected taoshan"
   device=taoshan
-  ;;
-  3) echo "Selected both"
-  device=both
   ;;
   fixpython) echo "Fixin'.."
   rm -rf venv
@@ -130,13 +76,42 @@ esac
 echo " "
 echo " "
 
-displaymenu
-prepareman
+echo "Selected device: $device"
+echo "========================"
+case "$device" in
+  taoshan)
+echo "1. MM(6.0.1)"
+echo "2. LP(5.1)"
+  ;;
+  grouper)
+echo "1. MM(6.0.1)"
+echo "2. KK(4.4.4)"
+  ;;
+esac
+echo -n "Select the version: "
+read choise
+if [ "$device" == "grouper" ] then
+  case "$choise" in
+    1) romver=mm
+    ;;
+    2) romver=kk
+    ;;
+  esac
+elif [ "$device" == "taoshan" ]; then
+  case "$choise" in
+    1) romver=mm
+    ;;
+    2) romver=lp
+    ;;
+  esac
+fi
 
-echo -n "Setup vendor and device specific files? [Y/N] "
+manifestprepare
+
+echo -n "All the preparation was successful, do you want to proceed? [Y/N]: "
 read menu
 case "$menu" in
-  y|Y) prepare
+  y|Y) manifestdownload
   ;;
   n|N) exit
   ;;
@@ -147,6 +122,5 @@ esac
 
 if [ $IFARCHLINUX == true ]; then
 echo "Creating python venv"
-sleep 2
 pythonvenv
 fi
