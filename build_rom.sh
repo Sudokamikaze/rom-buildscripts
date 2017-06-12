87f953867b014007d58f514a68b29d83d2f96c48
sudokamikaze <pulshencode@outlook.com>
2017-06-12 17:31:18 +0300

    Remove unused scripts

--- build_rom.sh
+++ build_rom.sh
@@ -13,13 +13,8 @@ eval $(grep MON= ./config.buildscripts)
 
 case "$IFARCHLINUX" in
   true) source venv/bin/activate
-  if [ "$BUILDKITKAT" == "false" ]; then
     export PATH="/usr/lib/jvm/java-8-openjdk/bin:$PATH"
     export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
-  else
-    export PATH="/usr/lib/jvm/java-6-jre/jre/bin:$PATH"
-    export JAVA_HOME=/usr/lib/jvm/java-6-jre/jre
-  fi
   ;;
 esac
 
@@ -28,11 +23,6 @@ export USE_CCACHE=1
 export CCACHE_DIR="$CCACHEPATH"/.ccache
 export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches
 prebuilts/misc/linux-x86/ccache/ccache -M "$CCACHESIZE"G
-fi
-
-if [ "$BUILDKITKAT" == "true" ]; then
-  pwdvar=$(pwd)
-  export PATH="$pwdvar/makedir:$PATH"
 fi
 
 if [ "$BLOCK_BASED_OTA" == "false" ]; then
